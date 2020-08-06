ae85624c9cf5ec66b08e3e66a8a45abc9d2f172c
TaeHoon <thkim9373@gmail.com>
2020-08-07 00:42:41 +0900

    Save commit.

--- app/src/main/java/com/hoony/testcodesample/example1/Animal.kt
+++ app/src/main/java/com/hoony/testcodesample/example1/Animal.kt
@@ -4,4 +4,14 @@ data class Animal(
     val name: String = "",
     val age: Int = 0,
     val isFly: Boolean = false
-)
+) {
+    fun canFly(species: Species): Boolean =
+        when (species) {
+            Species.BIRD -> true
+            else -> false
+        }
+}
+
+enum class Species {
+    DOG, CAT, BIRD
+}
--- app/src/test/java/com/hoony/testcodesample/example1/Example1UnitTest.kt
+++ app/src/test/java/com/hoony/testcodesample/example1/Example1UnitTest.kt
@@ -3,22 +3,37 @@ package com.hoony.testcodesample.example1
 import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
+// Mockito 를 사용하려면 요렇게 추가함
+// @RunWith(AndroidJUnit4::class) 를 붙여도 동작하긴 한다고 함.
 @RunWith(MockitoJUnitRunner::class)
 class Example1UnitTest {
 
-    @Test
+    @Test   // 요게 있으면 테스트 메소드라고 인식
     fun mockTest() {
-        val animal: Animal = Mockito.mock(Animal::class.java)
+        // animal 클래스를 mocking 하여 객체로 만듦
+        val animal: Animal = mock(Animal::class.java)
 
-        Mockito.`when`(animal.name).thenReturn("Puppy")
-        Mockito.`when`(animal.age).thenReturn(15)
-        Mockito.`when`(animal.isFly).thenReturn(false)
+        // 이 객체의 getName() 이 호출되면 "Puppy"라는 값을 리턴하도록 함
+        `when`(animal.name).thenReturn("Puppy")
+        `when`(animal.age).thenReturn(15)
+        `when`(animal.isFly).thenReturn(false)
 
+        // animal.name 이 호출되었을 때, 값이 "Puppy" 임을 검사
         assertEquals("Puppy", animal.name)
         assertEquals(15, animal.age)
         assertEquals(false, animal.isFly)
+    }
+
+    @Test
+    fun test2() {
+        val animal: Animal = mock(Animal::class.java)
+
+        `when`(animal.canFly(ArgumentMatchers.any())).thenReturn(true)
+
+        assertEquals(true, animal.canFly(Species.BIRD))
     }
 }
